231a35d29ed1939817b1d877dceb833316ca6c60
Keshav Chauhan <keshavchauhan1312@gmail.com>
2025-10-06 15:51:58 +0530

    feat: resize avatar images to 96x96px PNG during build

--- src/utils/buildPage.js
+++ src/utils/buildPage.js
@@ -8,6 +8,9 @@ const generateHTML = require('./generateHTML');
 const processCSS = require('./processCSS');
 const generateOGImage = require('./generateOGImage');
 const generateQR = require('./generateQR');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
 
 /**
  * Build all page components from configuration
@@ -31,6 +34,11 @@ const buildPage = async (config) => {
         // Generate QR Code (async)
         const qrImage = await generateQR(config.url);
         
+        // Process avatar
+        if (config.avatar && config.avatar.path) {
+            await processAvatar(config.avatar.path, config.outputDir);
+        }
+
         return {
             html,
             css,
@@ -42,5 +50,21 @@ const buildPage = async (config) => {
         throw new Error(`Failed to build page: ${error.message}`);
     }
 };
+
+async function processAvatar(avatarPath, outputDir) {
+  const ext = path.extname(avatarPath).toLowerCase();
+  const allowed = ['.png', '.jpg', '.jpeg', '.webp'];
+  const skipResize = ['.svg', '.gif'];
+  const outputAvatar = path.join(outputDir, 'avatar.png');
+
+  if (allowed.includes(ext)) {
+    await sharp(avatarPath)
+      .resize(96, 96, { fit: 'cover' })
+      .png()
+      .toFile(outputAvatar);
+  } else if (skipResize.includes(ext)) {
+    fs.copyFileSync(avatarPath, path.join(outputDir, path.basename(avatarPath)));
+  }
+}
 
 module.exports = buildPage;
